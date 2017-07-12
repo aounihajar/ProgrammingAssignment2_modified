ae222f59abbb97e492b65bb93380afe56b63eaea
Aouni Hajar <aouni@Aounis-Mac-mini.local>
2017-07-12 21:43:28 +0300

    R Programming, week 3 comment

---
+++ cachematrix.R
@@ -0,0 +1,43 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function (y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function () x
+        setinverse <- function (inverse) i <<- inverse
+
+        
+        getinverse <- function () i
+        list (set = set, get = get,
+              setinverse = setinverse, getinverse = getinverse)
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse ()
+        if (!is.null (i)) {
+                message ("Inverse is cached")
+                return (i)
+        }
+        data <- x$get()
+        i <- solve (data)
+        x$setinverse (i)
+        i
+}
+
+#testing a sample
+z <- matrix(c(4,2,7,6), 2, 2)
+solve (z)
+
+#testing with caching function
+cacheSolve(makeCacheMatrix (z))
